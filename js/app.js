a7b4b05af2c4e52f1ea62bfc31d15344e2796bb6
Dylan Bailey <wearesleepless@gmail.com>
2022-10-11 12:00:49 -0400

    Navbar

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-let options = {
-  strings: ["code.", "design.", "create."],
-  typeSpeed: 150,
-  backSpeed: 150,
-  loop: true
-};
+// let options = {
+//   strings: ["code.", "design.", "create."],
+//   typeSpeed: 150,
+//   backSpeed: 150,
+//   loop: true
+// };
 
-let typed = new Typed('.auto-type', options);
+// let typed = new Typed('.auto-type', options);
