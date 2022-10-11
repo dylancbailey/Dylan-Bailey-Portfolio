190ec9b7f7b30fd4a06e551f4b3f823d5550e01c
Dylan Bailey <wearesleepless@gmail.com>
2022-10-11 17:18:48 -0400

    Add hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
-let options = {
-  strings: ["Developer", "Engineer", "Designer"],
-  typeSpeed: 150,
-  backSpeed: 150,
-  loop: true
-};
+// new Typed('.auto-type', {
+//   strings: ['One', 'Two', 'Three'],
+//   typeSpeed: 150,
+//   backSpeed: 150,
+//   loop: true
+// });
 
-let typed = new Typed('.auto-type', options);
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav-list');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  nav.forEach(e => {
+    console.log(e.target);
+  });
+});
